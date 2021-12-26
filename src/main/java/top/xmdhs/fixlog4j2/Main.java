4f40a365dd892b1408754f934015696f39b7186a
xmdhs <xmdhss@gmail.com>
2021-12-27 00:29:47 +0800

    fix

--- src/main/java/top/xmdhs/fixlog4j2/Main.java
+++ src/main/java/top/xmdhs/fixlog4j2/Main.java
@@ -1,27 +1,31 @@
 package top.xmdhs.fixlog4j2;
 
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.LoaderClassPath;
 
-import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
+import java.io.ByteArrayInputStream;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 public class Main {
     public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new ClassFileTransformer() {
-            @Override
-            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-                if (className.equals("org/apache/logging/log4j/core/lookup/JndiLookup")) {
-                    ClassReader cr = new ClassReader(classfileBuffer);
-                    ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
-                    cr.accept(new MethodChangeClassAdapter(cw), ClassReader.EXPAND_FRAMES);
-                    return cw.toByteArray();
+        inst.addTransformer((ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) -> {
+            if (className.equals("org/apache/logging/log4j/core/lookup/JndiLookup"))
+                try {
+                    ClassPool p = ClassPool.getDefault();
+                    p.appendClassPath(new LoaderClassPath(loader));
+                    CtClass c = p.makeClass(new ByteArrayInputStream(classfileBuffer));
+                    CtMethod[] lookups = c.getDeclaredMethods("lookup");
+                    for (CtMethod v : lookups){
+                        v.setBody("return null;");
+                    }
+                    return c.toBytecode();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                return null;
-            }
+            return null;
         });
     }
 }
-
