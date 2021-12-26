6d2b5819f3072b1e10375c2c436b13d0714f996a
xmdhs <xmdhss@gmail.com>
2021-12-27 00:31:02 +0800

    fix

--- src/main/java/top/xmdhs/fixlog4j2/Main.java
+++ src/main/java/top/xmdhs/fixlog4j2/Main.java
@@ -12,19 +12,20 @@ import java.security.ProtectionDomain;
 public class Main {
     public static void premain(String agentArgs, Instrumentation inst) {
         inst.addTransformer((ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) -> {
-            if (className.equals("org/apache/logging/log4j/core/lookup/JndiLookup"))
+            if (className.equals("org/apache/logging/log4j/core/lookup/JndiLookup")) {
                 try {
                     ClassPool p = ClassPool.getDefault();
                     p.appendClassPath(new LoaderClassPath(loader));
                     CtClass c = p.makeClass(new ByteArrayInputStream(classfileBuffer));
                     CtMethod[] lookups = c.getDeclaredMethods("lookup");
-                    for (CtMethod v : lookups){
+                    for (CtMethod v : lookups) {
                         v.setBody("return null;");
                     }
                     return c.toBytecode();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
             return null;
         });
     }
